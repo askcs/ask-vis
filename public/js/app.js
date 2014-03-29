21eade234d145d0c6ca8c68afdb7e79b32898951
Cengizism <culusoy@ask-cs.com>
2014-03-29 17:28:30 +0100

    vis directive is in place

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,107 @@
 'use strict';
 
-// Declare app level module which depends on filters, and services
-
-angular.module('myApp', [
-  'myApp.controllers',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'ngRoute'
+angular.module('AskVis', [
+  'ngResource'
 ]).
-config(function ($routeProvider, $locationProvider) {
-  $routeProvider.
-    when('/view1', {
-      templateUrl: 'partials/partial1',
-      controller: 'MyCtrl1'
-    }).
-    when('/view2', {
-      templateUrl: 'partials/partial2',
-      controller: 'MyCtrl2'
-    }).
-    otherwise({
-      redirectTo: '/view1'
-    });
 
-  $locationProvider.html5Mode(true);
-});
+value('version', '0.1.0').
+
+controller('AppCtrl',[
+    '$scope', 'Data',
+    function ($scope, Data)
+    {
+      $scope.items = [
+        {id: 1, content: 'item 1', start: '2013-04-20'},
+        {id: 2, content: 'item 2', start: '2013-04-14'}
+      ];
+
+      Data.fetch('items')
+        .then(function (result)
+        {
+          $scope.items = result.items;
+        });
+    }
+  ]
+).
+
+directive('timeLine', [
+    function ()
+    {
+      return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+          items: '='
+        },
+        link: function (scope, element, attrs)
+        {
+          new vis.Timeline(element[0], scope.items || [], {});
+        }
+      }
+    }
+  ]
+).
+
+factory('Data', [
+    '$resource', '$q',
+    function ($resource, $q)
+    {
+      var Data = $resource('/:action/:level',
+        {},
+        {
+          items: {
+            method: 'GET',
+            params: {
+              action: 'api',
+              level:  'items'
+            }
+          }
+        }
+      );
+
+      Data.prototype.fetch = function (proxy, params, data, callback)
+      {
+        var deferred = $q.defer();
+
+        params = params || {};
+
+        try
+        {
+          Data[proxy](
+            params,
+            data,
+            function (result)
+            {
+              if (callback && callback.success) callback.success.call(this, result);
+
+              deferred.resolve(result);
+            },
+            function (result)
+            {
+              if (callback && callback.error) callback.error.call(this, result);
+
+              deferred.resolve({error: result});
+            }
+          );
+        }
+        catch (err) { console.warn('Error!: ', err); }
+
+        return deferred.promise;
+      };
+
+      return new Data();
+    }
+  ]
+).
+
+filter('interpolate', [
+    'version',
+    function (version)
+    {
+      return function (text)
+      {
+        return String(text).replace(/\%VERSION\%/mg, version);
+      };
+    }
+  ]
+);
