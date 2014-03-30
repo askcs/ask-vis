ec7f7bdf9994bde8bd3e61094681885e07f3ee06
Cengizism <culusoy@ask-cs.com>
2014-03-30 20:08:51 +0200

    stable version before deployment

--- public/js/app.js
+++ public/js/app.js
@@ -5,13 +5,20 @@ angular.module('AskVis', [
 ]).
 
 constant('options', {
+    debug: true,
     align: 'center',
     autoResize: true,
     editable: true,
     // now: moment().minutes(0).seconds(0).milliseconds(0),
-    start: null, // Date | Number | String - now.clone().add('days', -3) - new Date(Date.now() - 1000 * 60 * 60 * 24)
+    start: null,
+    // Date | Number | String
+    // now.clone().add('days', -3)
+    // new Date(Date.now() - 1000 * 60 * 60 * 24)
     // start: new Date(Date.now() - 1000 * 60 * 60 * 24 * 31 * 2),
-    end: null, // Date | Number | String - now.clone().add('days', 11) - new Date(Date.now() + 1000 * 60 * 60 * 24 * 6)
+    end: null,
+    // Date | Number | String
+    // now.clone().add('days', 11)
+    // new Date(Date.now() + 1000 * 60 * 60 * 24 * 6)
     // end: new Date(Date.now() + 1000 * 60 * 60 * 24 * 31 * 2),
     height: null,
     width: '100%',
@@ -25,40 +32,21 @@ constant('options', {
     orientation: 'bottom',
     padding: 5,
     selectable: true,
-    showCurrentTime: false,
-    showCustomTime: false,
+    showCurrentTime: true,
+    showCustomTime: true,
     showMajorLabels: true,
     showMinorLabels: true,
     type: 'box', // dot, point
-    zoomMin: 10, // 1000 * 60 * 60 * 24,          // a day
-    zoomMax: 1000 * 60 * 60 * 24 * 30 * 12 * 3,  // three years
-    _zoomMax: 315360000000000 // 10,000 years
+    zoomMin: 10, // 1000 * 60 * 60 * 24, // a day
+    zoomMax: 1000 * 60 * 60 * 24 * 30 * 12 * 3  // three years
+    // zoomMax: 315360000000000 // 10,000 years
   }).
 
 controller('AppCtrl',[
     '$scope', 'Data',
     function ($scope, Data)
     {
-      $scope.items = [
-        {id: 1, content: 'item 1', start: '2013-04-20'},
-        {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
-        {id: 5, content: 'item7<br><a href="http://visjs.org" target="_blank">click here</a>', start: '2013-04-25'}
-      ];
-
-      $scope.items = {
-        team_1: [
-          {id: 1, content: 'item 1', start: '2013-04-20'},
-          {id: 2, content: 'item 2', start: '2013-04-14'}
-        ],
-        team_2: [
-          {id: 3, content: 'item 3', start: '2013-04-18'},
-          {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'}
-        ],
-        team_3: [
-          {id: 5, content: 'item 5', start: '2013-04-25'},
-          {id: 6, content: 'item 6', start: '2013-04-27'}
-        ]
-      };
+      $scope.items = [];
 
       $scope.loadDataSet = function (num)
       {
@@ -69,25 +57,11 @@ controller('AppCtrl',[
           });
       };
 
-      // $scope.loadDataSet(1);
+      $scope.loadDataSet(1);
 
       $scope.customDate = new Date(Date.now()).toISOString().substr(0, 10);
 
       $scope.timeline = {};
-
-
-
-      // var count = 0;
-      // for (var k in data) { if (data.hasOwnProperty(k)) { ++count; }}
-
-      // console.log('data is ->', angular.isArray(data), count);
-
-
-
-
-
-
-
     }
   ]
 ).
@@ -101,29 +75,38 @@ directive('timeLine', [
         replace: true,
         scope: {
           items: '=',
-          timeline: '='
+          timeline: '=',
+          customTime: '='
         },
         link: function (scope, element, attrs)
         {
-
           var callbacks = {
-            // Provide a custom sort function to order the items. The order of the items
-            // is determining the way they are stacked. The function order is called with
-            // two parameters, both of type `vis.components.items.Item`.
+            /**
+             * Provide a custom sort function to order the items. The order of the items
+             * is determining the way they are stacked. The function order is called with
+             * two parameters, both of type `vis.components.items.Item`.
+             */
             order: function ()
             {
-              // console.log('order callback!');
+              if (options.debug)
+                console.log('order callback!');
             },
 
-            // String | Function
-            // Order the groups by a field name or custom sort function.
-            // By default, groups are not ordered.
+            /**
+             * String | Function
+             * Order the groups by a field name or custom sort function.
+             * By default, groups are not ordered.
+             */
             groupOrder: function ()
             {
             },
 
-            // Callback function triggered when an item is about to be added:
-            // when the user double taps an empty space in the Timeline.
+            /**
+             * Callback function triggered when an item is about to be added:
+             * when the user double taps an empty space in the Timeline.
+             * @param item
+             * @param callback
+             */
             onAdd: function (item, callback)
             {
               item.content = prompt('Enter text content for new item:', item.content);
@@ -138,8 +121,12 @@ directive('timeLine', [
               }
             },
 
-            // Callback function triggered when an item has been moved:
-            // after the user has dragged the item to an other position.
+            /**
+             * Callback function triggered when an item has been moved:
+             * after the user has dragged the item to an other position.
+             * @param item
+             * @param callback
+             */
             onMove: function (item, callback)
             {
               if (confirm('Do you really want to move the item to\n' +
@@ -153,8 +140,12 @@ directive('timeLine', [
               }
             },
 
-            // Callback function triggered when an item is about to be updated,
-            // when the user double taps an item in the Timeline.
+            /**
+             * Callback function triggered when an item is about to be updated,
+             * when the user double taps an item in the Timeline.
+             * @param item
+             * @param callback
+             */
             onUpdate: function (item, callback)
             {
               item.content = prompt('Edit items text:', item.content);
@@ -169,8 +160,12 @@ directive('timeLine', [
               }
             },
 
-            // Callback function triggered when an item is about to be removed:
-            // when the user tapped the delete button on the top right of a selected item.
+            /**
+             * Callback function triggered when an item is about to be removed:
+             * when the user tapped the delete button on the top right of a selected item.
+             * @param item
+             * @param callback
+             */
             onRemove: function (item, callback)
             {
               if (confirm('Remove item ' + item.content + '?'))
@@ -186,128 +181,70 @@ directive('timeLine', [
 
           angular.extend(options, callbacks);
 
-
-
-
-//          var data = new vis.DataSet({
-//            convert: {
-//              start: 'Date',
-//              end: 'Date'
-//            }
-//          });
-//
-//          data.on('*', function (event, properties)
-//          {
-//            // console.log('event=' + JSON.stringify(event) + ', ' + 'properties=' + JSON.stringify(properties));
-//          });
-//
-//          data.add(scope.items);
-
-          // var _timeline = new vis.Timeline(element[0], data, options);
-
-
-
-
-
-          var groups = new vis.DataSet();
-
-          var items = new vis.DataSet({
-            convert: {
-              start: 'Date',
-              end: 'Date'
-            }
-          });
-
-          var id = 0;
-
-          // console.log('items ->', scope.items);
-
-          angular.forEach(scope.items, function (_items, _group)
-          {
-            groups.add({
-              id:      id,
-              content: _group
-            });
-
-            angular.forEach(_items, function (item)
-            {
-              var _item = {
-                id: item.id,
-                group: id,
-                content: item.content,
-                start: item.start
-                //,
-                //type: 'box'
-              };
-
-              if (item.hasOwnProperty('end')) { _item.end = item.end; }
-
-              items.add(_item);
-            });
-
-            id++;
-          });
-
-
-          options.groupOrder = 'content';
-
           var _timeline = new vis.Timeline(element[0]);
           _timeline.setOptions(options);
-          _timeline.setGroups(groups);
-          _timeline.setItems(items);
 
+          function render (data)
+          {
+            var groups = new vis.DataSet();
 
-//          var now = moment().minutes(0).seconds(0).milliseconds(0);
-//          var groupCount = 3;
-//          var itemCount = 20;
-//
-//          // create a data set with groups
-//          var names = ['John', 'Alston', 'Lee', 'Grant'];
-//
-//          var groups = new vis.DataSet();
-//
-//          for (var g = 0; g < groupCount; g++)
-//          {
-//            groups.add({id: g, content: names[g]});
-//          }
-//
-//          // create a dataset with items
-//          var items = new vis.DataSet();
-//
-//          for (var i = 0; i < itemCount; i++)
-//          {
-//            var start = now.clone().add('hours', Math.random() * 200);
-//            var group = Math.floor(Math.random() * groupCount);
-//
-//            items.add({
-//              id: i,
-//              group: group,
-//              content: 'item ' + i +
-//                ' <span style="color:#97B0F8;">(' + names[group] + ')</span>',
-//              start: start,
-//              type: 'box'
-//            });
-//          }
-//
-//          options.groupOrder = 'content';
-//
-//          var _timeline = new vis.Timeline(element[0]);
-//          _timeline.setOptions(options);
-//          _timeline.setGroups(groups);
-//          _timeline.setItems(items);
+            var items = new vis.DataSet({
+              convert: {
+                start: 'Date',
+                end: 'Date'
+              }
+            });
 
-          console.log('groups ->', groups);
+            items.on('*', function (event, properties)
+            {
+              if (options.debug)
+                console.log('event=' +
+                  angular.toJson(event) + ', ' +
+                  'properties=' + angular.toJson(properties));
+            });
 
-          console.log('items ->', items);
+            if (angular.isArray(data))
+            {
+              items.add(data);
 
+              _timeline.setItems(items);
+            }
+            else
+            {
+              var id = 0;
 
+              angular.forEach(data, function (_items, _group)
+              {
+                groups.add({
+                  id:      id,
+                  content: _group
+                });
 
+                angular.forEach(_items, function (item)
+                {
+                  var _item = {
+                    id: item.id,
+                    group: id,
+                    content: item.content,
+                    start: item.start
+                  };
 
+                  if (item.hasOwnProperty('end')) { _item.end = item.end; }
 
+                  items.add(_item);
+                });
 
+                id++;
+              });
 
+              options.groupOrder = 'content';
 
-          // scope.$watch('items', function (data) { _timeline.setItems(data) }, true);
+              _timeline.setGroups(groups);
+              _timeline.setItems(items);
+            }
+          }
+
+          scope.$watch('items', function (data) { render(data); }, true);
 
           scope.timeline = {
             customDate: _timeline.getCustomTime(),
@@ -321,29 +258,33 @@ directive('timeLine', [
 
           _timeline.on('rangechange', function (period)
           {
-            // console.log('rangechange: start -> ', period.start, ' end -> ', period.end);
+            if (options.debug)
+              console.log('rangechange: start -> ', period.start, ' end -> ', period.end);
           });
 
           _timeline.on('rangechanged', function (period)
           {
-            // console.log('rangechanged: start -> ', period.start, ' end -> ', period.end);
+            if (options.debug)
+              console.log('rangechanged: start -> ', period.start, ' end -> ', period.end);
           });
 
           _timeline.on('select', function (selected)
           {
-            // console.log('select: items -> ', selected.items);
+            if (options.debug)
+              console.log('select: items -> ', selected.items);
           });
 
           _timeline.on('timechange', function (period)
           {
-            scope.timeliner.customDate = period.time;
+            scope.timeline.customDate = period.time;
 
             scope.$apply();
           });
 
           _timeline.on('timechanged', function (period)
           {
-            // console.log('timechanged: -> ', period.time);
+            if (options.debug)
+              console.log('timechanged: -> ', period.time);
           });
         }
       }
@@ -399,18 +340,6 @@ factory('Data', [
       };
 
       return new Data();
-    }
-  ]
-).
-
-filter('interpolate', [
-    'version',
-    function (version)
-    {
-      return function (text)
-      {
-        return String(text).replace(/\%VERSION\%/mg, version);
-      };
     }
   ]
 );
--- routes/api.js
+++ routes/api.js
@@ -3,34 +3,34 @@ exports.items = function (req, res)
 {
   var data;
 
-  if (req.query.set == 2)
+  if (req.query.set == 1)
   {
     data = {
       items: [
-        {id: 1, content: 'item 1', start: '2013-04-20'},
-        {id: 2, content: 'item 2', start: '2013-04-14'},
-        {id: 3, content: 'item 3', start: '2013-04-18'},
-        {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
-        {id: 5, content: 'item 5', start: '2013-04-25'},
-        {id: 6, content: 'item 6', start: '2013-04-27'}
+        {id: 1, content: 'item 1', start: '2014-03-20'},
+        {id: 2, content: 'item 2', start: '2014-03-14'},
+        {id: 3, content: 'item 3', start: '2014-03-18'},
+        {id: 4, content: 'item 4', start: '2014-03-16', end: '2014-03-19'},
+        {id: 5, content: 'item 5', start: '2014-03-25'},
+        {id: 6, content: 'item 6', start: '2014-03-27'}
       ]
     };
   }
-  else if (req.query.set == 1)
+  else if (req.query.set == 2)
   {
     data = {
       items: {
         team_1: [
-          {id: 1, content: 'item 1', start: '2013-04-20'},
-          {id: 2, content: 'item 2', start: '2013-04-14'}
+          {id: 1, content: 'item 1', start: '2014-03-20'},
+          {id: 2, content: 'item 2', start: '2014-03-14'}
         ],
         team_2: [
-          {id: 3, content: 'item 3', start: '2013-04-18'},
-          {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'}
+          {id: 3, content: 'item 3', start: '2014-03-18'},
+          {id: 4, content: 'item 4', start: '2014-03-16', end: '2014-03-19'}
         ],
         team_3: [
-          {id: 5, content: 'item 5', start: '2013-04-25'},
-          {id: 6, content: 'item 6', start: '2013-04-27'}
+          {id: 5, content: 'item 5', start: '2014-03-25'},
+          {id: 6, content: 'item 6', start: '2014-03-27'}
         ]
       }
     };
