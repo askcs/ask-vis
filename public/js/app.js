1882eea7f13627e991128224959be3930f0d07a7
Cengizism <culusoy@ask-cs.com>
2014-05-10 15:28:44 +0200

    dropped lawnchair and underscore

--- public/js/app.js
+++ public/js/app.js
@@ -8,55 +8,48 @@
 
 angular.module('AskVis', ['ngResource', 'NgVis']).
 
-controller('AppCtrl', [
-    '$scope', 'Data', 'Store',
-    function ($scope, Data, Store)
+  controller(
+  'AppCtrl', [
+    '$scope', 'Data',
+    function ($scope, Data)
     {
       var debug = false;
 
-      Store = Store('data');
-
-      /**
-       * Data
-       */
       $scope.items = [];
 
       $scope.loadDataSet = function (num)
       {
-        Data.fetch('items', { set: num })
-          .then(function (result)
+        Data.fetch(
+          'items',
+          { set: num })
+          .then(
+          function (result)
           {
-            Store.save(result, 'items');
-
             $scope.items = result.items;
-          });
+          }
+        );
       };
 
-      Store.get('items', function (data)
-      {
-        if (data.hasOwnProperty('items'))
-        {
-          $scope.items = data.items;
-        }
-        else
-        {
-          $scope.loadDataSet(2);
-        }
-      });
+      $scope.loadDataSet(2);
 
       $scope.simplifyItems = function (items)
       {
         var simplified = [];
 
-        angular.forEach(items, function (group, label)
-        {
-          angular.forEach(group, function (item)
+        angular.forEach(
+          items,
+          function (group, label)
           {
-            item.group = label;
+            angular.forEach(
+              group,
+              function (item)
+              {
+                item.group = label;
 
-            simplified.push(item);
-          });
-        });
+                simplified.push(item);
+              });
+          }
+        );
 
         return simplified;
       };
@@ -69,26 +62,31 @@ controller('AppCtrl', [
         select: function (selected)
         {
           if (debug)
+          {
             console.log('selected items: ', selected.items);
+          }
 
           var items = $scope.simplifyItems($scope.items);
 
           var format = 'YYYY-MM-DDTHH:mm';
 
-          angular.forEach(items, function (item)
-          {
-            if (item.id == selected.items[0])
+          angular.forEach(
+            items,
+            function (item)
             {
-              $scope.slot = {
-                id:     item.id,
-                start:  moment(item.start).format(format),
-                end:    (item.end) ? moment(item.end).format(format) : null,
-                content:item.content
-              };
+              if (item.id == selected.items[0])
+              {
+                $scope.slot = {
+                  id:      item.id,
+                  start:   moment(item.start).format(format),
+                  end: (item.end) ? moment(item.end).format(format) : null,
+                  content: item.content
+                };
 
-              $scope.$apply();
+                $scope.$apply();
+              }
             }
-          });
+          );
         },
 
         range: {},
@@ -97,18 +95,27 @@ controller('AppCtrl', [
         {
           this.range = $scope.timeline.getWindow();
 
-          if (!$scope.$$phase) { $scope.$apply(); }
+          if (! $scope.$$phase)
+          {
+            $scope.$apply();
+          }
 
           if (debug)
-            console.log('rangeChange: start-> ',
+          {
+            console.log(
+              'rangeChange: start-> ',
               period.start, ' end-> ', period.end);
+          }
         },
 
         rangeChanged: function (period)
         {
           if (debug)
-            console.log('rangeChange(d): start-> ',
+          {
+            console.log(
+              'rangeChange(d): start-> ',
               period.start, ' end-> ', period.end);
+          }
         },
 
         customTime: null,
@@ -116,18 +123,24 @@ controller('AppCtrl', [
         timeChange: function (period)
         {
           if (debug)
-            console.log('timeChange: ', period.time);
-
-          $scope.$apply(function ()
           {
-            $scope.timeline.customTime = period.time;
-          });
+            console.log('timeChange: ', period.time);
+          }
+
+          $scope.$apply(
+            function ()
+            {
+              $scope.timeline.customTime = period.time;
+            }
+          );
         },
 
         timeChanged: function (period)
         {
           if (debug)
+          {
             console.log('timeChange(d): ', period.time);
+          }
         },
 
         slot: {
@@ -147,9 +160,11 @@ controller('AppCtrl', [
 
           move: function (item, callback)
           {
-            if (confirm('Do you really want to move the item to\n' +
-              'start: ' + item.start + '\n' +
-              'end: ' + item.end + '?')) {
+            if (confirm(
+                'Do you really want to move the item to\n' +
+                'start: ' + item.start + '\n' +
+                'end: ' + item.end + '?'))
+            {
               callback(item); // send back item as confirmation (can be changed
             }
             else
@@ -185,33 +200,6 @@ controller('AppCtrl', [
           }
         }
       };
-
-      /**
-       * Slot actions
-       */
-      $scope.actions = {
-        save: function ()
-        {
-          var items = $scope.items;
-
-          angular.forEach(items, function (group)
-          {
-            angular.forEach(group, function (item)
-            {
-              if (item.id == $scope.slot.id)
-              {
-                item.start    = $scope.slot.start;
-                item.end      = $scope.slot.end;
-                item.content  = $scope.slot.content;
-              }
-            })
-          });
-
-          $scope.items = items;
-
-          Store.save(items, 'items');
-        }
-      }
 
       $scope.getCustomTime = function ()
       {
@@ -250,11 +238,13 @@ controller('AppCtrl', [
   ]
 ).
 
-factory('Data', [
+  factory(
+  'Data', [
     '$resource', '$q',
     function ($resource, $q)
     {
-      var Data = $resource('/:action/:level',
+      var Data = $resource(
+        '/:action/:level',
         {},
         {
           items: {
@@ -292,7 +282,8 @@ factory('Data', [
             }
           );
         }
-        catch (err) { console.warn('Error!: ', err); }
+        catch (err)
+        { console.warn('Error!: ', err); }
 
         return deferred.promise;
       };
@@ -300,357 +291,4 @@ factory('Data', [
       return new Data();
     }
   ]
-).
-
-factory('Store', [
-  '$window', '$log', '$parse',
-    function($window, $log, $parse)
-    {
-      return function (name, config)
-      {
-        var LawnChair,
-            Store,
-            allAsArray,
-            allAsCollection,
-            array,
-            collection,
-            getDefault,
-            getEntryId,
-            idGetter,
-            isArray,
-            removeEntry,
-            saveEntry,
-            transformLoad,
-            transformSave,
-            updateArray,
-            updateCache,
-            updateCacheFromStorage;
-
-        getEntryId = function (entry)
-        {
-          var e;
-
-          try
-          {
-            return idGetter(entry);
-          }
-          catch (_error)
-          {
-            e = _error;
-            return null;
-          }
-        };
-
-        LawnChair = function (callback)
-        {
-          return new Lawnchair({
-            name: name
-          }, callback);
-        };
-
-        saveEntry = function (data, key)
-        {
-          var e, update;
-
-          key = key.toString();
-
-          if (angular.isObject(data) && data !== collection[key])
-          {
-            collection[key] = collection[key] || {};
-            angular.extend(collection[key], data);
-          }
-          else
-          {
-            collection[key] = data;
-          }
-
-          update = {
-            key: key,
-            value: transformSave(collection[key])
-          };
-
-          try
-          {
-            LawnChair(function() {
-              this.save(update);
-            });
-          }
-          catch (_error)
-          {
-            e = _error;
-
-            if (e.name === 'QUOTA_EXCEEDED_ERR' || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
-            {
-              $window.localStorage.clear();
-            }
-
-            $log.info('LocalStorage Exception ==> ' + e.message);
-          }
-        };
-
-        updateArray = function (data)
-        {
-          array.length = 0;
-
-          _.each(data, function(o)
-          {
-            array.push(o);
-          });
-
-          return array;
-        };
-
-        updateCache = function (obj, key)
-        {
-          if (obj && angular.isObject(obj) && collection[key] && collection[key] !== obj)
-          {
-            angular.extend(collection[key], obj);
-          }
-          else
-          {
-            collection[key] = obj;
-          }
-        };
-
-        updateCacheFromStorage = function (cache, storage)
-        {
-          if (storage)
-          {
-            if (angular.isObject(storage.value) && angular.isObject(cache))
-            {
-              angular.extend(cache, transformLoad(storage.value));
-            }
-            else
-            {
-              cache = transformLoad(storage.value);
-            }
-
-            updateCache(cache, storage.key);
-          }
-
-          return cache;
-        };
-
-        allAsCollection = function (callback)
-        {
-          LawnChair(function()
-          {
-            this.all(function(result)
-            {
-              angular.forEach(result, function (o)
-              {
-                updateCache(o.value, o.key);
-              });
-
-              if (callback)
-              {
-                callback(collection);
-              }
-            });
-          });
-
-          return collection;
-        };
-
-        allAsArray = function (callback)
-        {
-          return updateArray(allAsCollection(function (data)
-          {
-            updateArray(data);
-
-            if (callback)
-              callback(array);
-          }));
-        };
-
-        removeEntry = function (key)
-        {
-          delete collection[key];
-
-          LawnChair(function ()
-          {
-            this.remove(key);
-          });
-        };
-
-        getDefault = function (key)
-        {
-          var d;
-
-          if (collection[key])
-          {
-            return collection[key];
-          }
-          else
-          {
-            d = {};
-
-            idGetter.assign(d, key);
-
-            return d;
-          }
-        };
-
-        collection = {};
-
-        array = [];
-
-        isArray = config && config.isArray;
-        idGetter = $parse((config && config.entryKey ? config.entryKey : 'id'));
-
-        // TODO: Still needed? since there is no config anymore
-        transformSave = (config && config.transformSave ? config.transformSave : angular.identity);
-        transformLoad = (config && config.transformLoad ? config.transformLoad : angular.identity);
-
-        Store = {
-          collection: collection,
-
-          save: function (data, key, clear)
-          {
-            var newIds;
-
-            if (!data)
-            {
-              data = collection;
-              key = null;
-            }
-
-            if (angular.isArray(data))
-            {
-              angular.forEach(data, function (e, index)
-              {
-                saveEntry(e, getEntryId(e) || index);
-              });
-            }
-            else if (key || (data && getEntryId(data)))
-            {
-              saveEntry(data, key || getEntryId(data));
-            }
-            else
-            {
-              angular.forEach(data, saveEntry);
-            }
-
-            if (clear)
-            {
-              newIds = (angular.isArray(data) ?
-                        _.chain(data).map(getEntryId).map(String).value() :
-                        _.keys(data));
-
-              _.chain(collection)
-                .keys()
-                .difference(newIds)
-                .each(removeEntry);
-
-              _.chain(collection)
-                .filter(function (entry)
-                {
-                  return !getEntryId(entry);
-                }
-              ).keys().each(removeEntry);
-            }
-
-            if (isArray)
-            {
-              updateArray(collection);
-            }
-          },
-
-          batch: function (keys, target, callback)
-          {
-            var cache;
-
-            cache = _.chain(keys).map(function (k)
-            {
-              return getDefault(k);
-            }).value();
-
-            if (target && angular.isArray(target))
-            {
-              target.length = 0;
-
-              _.each(cache, function (o)
-              {
-                target.push(o);
-              });
-            }
-            else
-            {
-              target = cache;
-            }
-
-            LawnChair(function ()
-            {
-              this.get(keys, function (result)
-              {
-                var i;
-
-                if (result)
-                {
-                  i = result.length - 1;
-
-                  while (i >= 0)
-                  {
-                    target[i] = updateCacheFromStorage(target[i], result[i]);
-
-                    i--;
-                  }
-                }
-                if (callback)
-                  callback(target);
-              });
-            });
-            return target;
-          },
-
-          get: function (key, callback)
-          {
-            var value;
-
-            value = getDefault(key);
-
-            LawnChair(function ()
-            {
-              this.get(key, function (result)
-              {
-                if (result)
-                  value = updateCacheFromStorage(value, result);
-
-                if (callback)
-                  callback(value);
-              });
-            });
-
-            return value;
-          },
-
-          all: (isArray ? allAsArray : allAsCollection),
-
-          remove: removeEntry,
-
-          nuke: function ()
-          {
-            LawnChair(function()
-            {
-              this.nuke();
-            });
-          },
-
-          destroy: function ()
-          {
-            var key;
-
-            for (key in collection)
-              delete collection[key];
-
-            LawnChair(function ()
-            {
-              this.nuke();
-            });
-          }
-        };
-
-        return Store;
-      };
-    }
-]);
+);
